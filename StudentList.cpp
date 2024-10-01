c8818f431d79fa3bccf4c62bbe0f2eaa33d60da7
U-BSD\476275 <476275@SHS-95W2NW3.beaverton.k12.or.us>
2024-10-01 11:48:30 -0700

    Added most features, still need to fix errors with invalid inputs from user in the Add() method

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,56 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
-
-/*
-  Create a struct.  It will have a first name (character array), a last name (character array), a number for the student id (int), and decimal for GPA (float).
-
- 
-
-Your program should have a vector of struct pointers passed by reference, or a vector pointer (which will point to a vector of struct pointers). (20 points)
-
- 
-
-Your program will allow you to read in students, print them out, and delete them.  Each of these functions should be in a different method.
-
- 
-
-When you type in: ADD
-
-You should create a new entry for a student.  After you type in add, the program should prompt for first name, last name, student id, and GPA.  That student should then be added to the vector of students (well, student pointers!). (20 points)
-
-
-When you type in: PRINT
-
-Your program should print out all the students currently stored.  For example:
-
- 
-
-Jason Galbraith, 487329, 5.00
-
-John Elstad, 903826, 3.25
-
-Kevin Jin, 468921, 0.12
-
- 
-
-The GPA should be shown to two decimal places. I recommend looking up how iterators work; regular for loops do not work with vectors. (20 points)
-
- 
-
-When you type in: DELETE
-
-Prompt the user for the student id number to delete, and remove that struct from the vector. Be sure to delete the data. (20 points)
-
- 
-
-By the way, your program should finish with a "QUIT" command.
- */
-
-//pointers with star point to the value
-//Without star points to spot in memory
-
 
 struct Student {
   char firstName[10];
@@ -59,65 +11,74 @@ struct Student {
   float gpa;
 };
 
+//Methods initizalized here 
+bool userSelection(vector<Student*> &);
 void Add(vector<Student*> &);
 void Print(vector<Student*> &);
 void Delete(vector<Student*> &);
-void Quit();
-
+void setLowerCase(char cstring[]);
 
 int main() {
-  cout << "test" << endl;
   vector<Student*> stuVec; //vector of student pointers 
 
   //Test student
-  Student* bob = new Student; //to make a student point you have to make a new one in the heap? So bob is a pointer to a student object
+  Student* bob = new Student; //Makes a student pointer 'bob' that points to a new Student object 
   bob->firstName[0] = 'b'; //now use -> operator to get the structure member with a pointer
- bob->firstName[1] = 'o';
- bob->firstName[2] = 'b';
- bob->firstName[3] = '\0';
- bob->lastName[0] = 'o';
- bob->lastName[1] = 'n';
- bob->lastName[2] = 'e';
- bob->lastName[3] = '\0';
- bob->id = 1234;
- bob->gpa = 4.00;
+  bob->firstName[1] = 'o';
+  bob->firstName[2] = 'b';
+  bob->firstName[3] = '\0';
+  bob->lastName[0] = 'o';
+  bob->lastName[1] = 'n';
+  bob->lastName[2] = 'e';
+  bob->lastName[3] = '\0';
+  bob->id = 1234;
+  bob->gpa = 4.00;
+  stuVec.push_back(bob);
 
- stuVec.push_back(bob);
  
- Print(stuVec);
-
-  bool validIn = false;
-  while (!validIn) {
-
- char usrChoice;
-  cout << "Create (c), Print out (p), or Delete (d) a student" << endl;
-  cin >> usrChoice;
-  //cin.get(usrChoice, 7);
-  //cin.get();
-
-  if (usrChoice == 'c') {
-    cout << "creates" << endl;
-    validIn = true;
-    Add(stuVec); 
-  } else if (usrChoice == 'p') {
-    cout << "pritns" << endl;
-    Print(stuVec);
-    validIn = true;
-  } else if (usrChoice == 'd') {
-    cout << "deltes" << endl;
-    Delete(stuVec);
-    validIn = true;
-  } else {
-    cout << "invalid input" << endl;
-    }
-  } 
-  
+  //Keep looping till the user inputs quit command
+  while (!userSelection(stuVec)); 
   
   return 0;
 }
 
+
+//Menu where user selects what they want to do.
+bool userSelection(vector<Student*> &stuVec) {
+  bool validIn = false;
+  while (!validIn) {
+    char input[7]; 
+    char usrChoice;
+    cout << "Create (c), Print out (p), or Delete (d) a student. Quit (q)" << endl;
+    
+    cin.get(input, 7);
+    setLowerCase(input); //set the letters to lowercase
+    cin.ignore();
+    //Checks which input the user did.
+    if (strcmp(input, "create") == 0 || strcmp(input, "c") == 0) {
+      cout << "creates" << endl;
+      validIn = true;
+      Add(stuVec);
+    } else if (strcmp(input, "print") == 0 || strcmp(input, "p") == 0) {
+      cout << "pritns" << endl;
+      Print(stuVec);
+      validIn = true;
+    } else if (strcmp(input, "delete") == 0 || strcmp(input, "d") == 0) {
+      cout << "deltes" << endl;
+      Delete(stuVec);
+      validIn = true;
+    } else if (strcmp(input, "quit") == 0 || strcmp(input, "q") == 0) {
+      return true;
+    } else {
+      cout << "invalid input" << endl;
+    }
+    //return true;
+  }
+  return false; 
+}
+
+//Adds a new student to the list, user assigns student data.
 void Add(vector<Student*> &stuVec) {
-  
   cout << "not implimented" << endl;
   Student* anon = new Student; //create a new student pointer
   cout << "Student first name: " << endl;
@@ -128,19 +89,25 @@ void Add(vector<Student*> &stuVec) {
   cin.get();
   cout << "Student id: " << endl;
   cin >> anon->id;
+  cin.ignore();
   cout << "Student gpa: " << endl;
   cin >> anon->gpa;
+  cin.ignore();
   stuVec.push_back(anon); //add it to the vector
 }
 
 //Prints out all of the currently stored student data.
 void Print(vector<Student*> &stuVec) { //pass in the the vector of student pointers (list of pointers to students)
+  if (stuVec.empty()) {
+    cout << "There are no students" << endl;
+  } else {
   //The iterator 'it' is a pointer to Student*, and Student* points to the Student objects 
   for (vector<Student*>::iterator it = stuVec.begin(); it != stuVec.end(); it++) {
     //print out the information for a student.
     cout.setf(ios::showpoint); //show zeros in the decimal places
     cout.precision(3); //to 2 decimal places
     cout << (*it)->firstName << " " << (*it)->lastName << ", " << (*it)->id << ", " << (*it)->gpa << endl;
+  }
   }
 }
 
@@ -172,8 +139,12 @@ void Delete(vector<Student*> &stuVec) {
   delete(stuVec.at(idIndex));
   stuVec.erase(stuVec.begin() + idIndex);
   }
-
-  void Quit() {
-    //quits the program.
-  } 
 }
+
+//Pass in an array by reference, to change all contents to lowercase
+ void setLowerCase(char cstring[]) {
+   for (int i = 0; i != '\0'; i++) { //loop until null character (end of cstring)
+     cstring[i] = tolower(cstring[i]); //set the char here to lowercase  
+   }
+ }
+
